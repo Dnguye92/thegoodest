f346a1f7480d7ce1c94ce96b62710ea8467dabf1
Dylan Nguyen <nguyendylan92@gmail.com>
2019-04-08 23:34:11 -0700

    remove server folder from public repo

--- server/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://dylan_nguyen:Meatball1@goodapi-c756a.mongodb.net/GoodDB?retryWrites=true', {
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
